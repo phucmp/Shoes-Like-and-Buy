49a371ee566498b0ed89594335825624ac2a9394
phucmp <phucmp@users.noreply.github.com>
2017-06-23 14:22:56 -0700

    Added heart for like and changed button text to unlike

--- StacksOnStacks/ViewController.swift
+++ StacksOnStacks/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heart: UIImageView!
+    @IBOutlet weak var LikeBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func likeBtnPressed(_ sender: Any) {
+        heart.isHidden = false
+        UIView.animate(withDuration: 2, animations: {self.heart.alpha = 0})
+        LikeBtn.setTitle("UNLIKE", for: .normal)
+    }
 
 }
 
